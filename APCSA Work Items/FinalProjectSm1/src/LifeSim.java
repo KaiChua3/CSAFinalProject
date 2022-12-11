24284d00076b1c4e90bde72520b8672c253dece1
KaiChua3 <91628674+KaiChua3@users.noreply.github.com>
2022-12-11 00:46:21 -0800

    Add files via upload

---
+++ APCSA Work Items/FinalProjectSm1/src/LifeSim.java
@@ -0,0 +1,12 @@
+public class LifeSim {
+    private double money;
+    private int health;
+    private int age;
+    
+    public LifeSim() {
+
+    }
+    public static void main(String args[]) {
+        System.out.println("Hello, and welcome to the life simulation. You will simulate life starting as either a college student or a working adult out of high school.");
+    }
+}
